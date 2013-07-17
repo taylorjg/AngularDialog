d986719ef67acf83f383931bacfe7f6b209e8b88
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-07-17 01:02:33 +0100

    Started to add a RESTful service

---
+++ AngularDialog/Domain/NameListEntry.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+
+namespace AngularDialog.Domain
+{
+    [DataContract]
+    public class NameListEntry
+    {
+        public NameListEntry(string firstName, string lastName, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public NameListEntry(int id, string firstName, string lastName, string email)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        [DataMember]
+        public int Id { get; private set; }
+
+        [DataMember]
+        public string FirstName { get; private set; }
+
+        [DataMember]
+        public string LastName { get; private set; }
+
+        [DataMember]
+        public string Email { get; private set; }
+    }
+}
---
+++ AngularDialog/Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+using AngularDialog.Api;
+using AngularDialog.Domain;
+using AngularDialog.Repositories;
+
+namespace AngularDialog
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new {id = RouteParameter.Optional});
+
+            // http://blog.ploeh.dk/2012/10/03/DependencyInjectioninASP.NETWebAPIwithCastleWindsor/
+            GlobalConfiguration.Configuration.Services.Replace(
+                typeof(IHttpControllerActivator),
+                new PoorMansCompositionRoot());
+        }
+    }
+
+    internal class PoorMansCompositionRoot : IHttpControllerActivator
+    {
+        private static INameListRepository _nameListRepository;
+
+        public IHttpController Create(
+            HttpRequestMessage request,
+            HttpControllerDescriptor controllerDescriptor,
+            Type controllerType)
+        {
+            if (controllerType == typeof (NameListController))
+            {
+                if (_nameListRepository == null)
+                {
+                    _nameListRepository = new InMemoryNameListRepository();
+                    _nameListRepository.Create(new NameListEntry("Jonathan", "Taylor", "jonathan.taylor@drllimited.co.uk"));
+                    _nameListRepository.Create(new NameListEntry("Michael", "Whiteside", "michael.whiteside@drllimited.co.uk"));
+                    _nameListRepository.Create(new NameListEntry("Russell", "Allen", "russell.allen@drllimited.co.uk"));
+                }
+
+                return new NameListController(_nameListRepository);
+            }
+
+            return null;
+        }
+    }
+}
