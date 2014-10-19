0b60461d60dccc3b8bdc9f4ad6f7d2e17a0cfd0f
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-10-20 00:17:04 +0100

    Got a second e2e test working in Protractor

--- AngularDialog/Tests/EndToEndTests/EndToEndTestSpec.js
+++ AngularDialog/Tests/EndToEndTests/EndToEndTestSpec.js
@@ -31,17 +31,34 @@
 	};
 
     describe("AngularDialog End-to-End Tests", function () {
-	
-		describe("main page", function () {
 
-			it("initially displays 2 items in the table", function () {
-				browser.get(urlWithTestIdentifier(1));
-			    checkNameListRows([
-			        ["1", "firstname1", "lastname1", "firstname1.lastname1@gmail.com"],
-			        ["2", "firstname2", "lastname2", "firstname2.lastname2@gmail.com"]
-			    ]);
-			});
-		});
-	});
+        describe("main page", function() {
+
+            it("initially displays 2 items in the table", function() {
+                browser.get(urlWithTestIdentifier(1));
+                checkNameListRows([
+                    ["1", "firstname1", "lastname1", "firstname1.lastname1@gmail.com"],
+                    ["2", "firstname2", "lastname2", "firstname2.lastname2@gmail.com"]
+                ]);
+            });
+        });
+        
+        describe("adding an item", function () {
+
+            it("clicking the ok button in the AddItem dialog box appends a new item to the table", function () {
+                browser.get(urlWithTestIdentifier(2));
+                element(by.id("addItemBtn")).click();
+                element(by.model("addItemDialogModel.item.FirstName")).sendKeys("firstname3");
+                element(by.model("addItemDialogModel.item.LastName")).sendKeys("lastname3");
+                element(by.model("addItemDialogModel.item.Email")).sendKeys("firstname3.lastname3@gmail.com");
+                element(by.id("okBtn")).click();
+                checkNameListRows([
+                    ["1", "firstname1", "lastname1", "firstname1.lastname1@gmail.com"],
+                    ["2", "firstname2", "lastname2", "firstname2.lastname2@gmail.com"],
+                    ["3", "firstname3", "lastname3", "firstname3.lastname3@gmail.com"]
+                ]);
+            });
+        });
+    });
 	
 }());
