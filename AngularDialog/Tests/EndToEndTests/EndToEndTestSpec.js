0f759664bd79abc50720e8f4ca0ea9e9dd9e704b
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-10-20 12:09:23 +0100

    All "editing an item" e2e tests have been Protractor-ised

--- AngularDialog/Tests/EndToEndTests/EndToEndTestSpec.js
+++ AngularDialog/Tests/EndToEndTests/EndToEndTestSpec.js
@@ -96,6 +96,29 @@
                 checkNumNameListRows(2);
             });
         });
+        
+        describe("editing an item", function () {
+
+            it("when an item is edited, the dialog controls are populated correctly", function () {
+                browser.get(urlWithTestIdentifier(3));
+                element.all(by.repeater("item in nameListModel.items")).get(1).element(by.css(".editBtn")).click();
+                expect(element(by.model("addItemDialogModel.item.FirstName")).getAttribute("value")).toBe("firstname2");
+                expect(element(by.model("addItemDialogModel.item.LastName")).getAttribute("value")).toBe("lastname2");
+                expect(element(by.model("addItemDialogModel.item.Email")).getAttribute("value")).toBe("firstname2.lastname2@gmail.com");
+            });
+
+            iit("an item can be edited", function () {
+                browser.get(urlWithTestIdentifier(3));
+                element.all(by.repeater("item in nameListModel.items")).get(1).element(by.css(".editBtn")).click();
+                element(by.model("addItemDialogModel.item.FirstName")).clear().sendKeys("firstname2-new");
+                element(by.model("addItemDialogModel.item.LastName")).clear().sendKeys("lastname2-new");
+                element(by.id("okBtn")).click();
+                checkNameListRows([
+                    ["1", "firstname1", "lastname1", "firstname1.lastname1@gmail.com"],
+                    ["2", "firstname2-new", "lastname2-new", "firstname2.lastname2@gmail.com"]
+                ]);
+            });
+        });
     });
 	
 }());
