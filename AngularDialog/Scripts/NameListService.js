7a531a21ad7e25adebe16f046d2b619bcc9065bc
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-10-21 10:09:46 +0100

    Simplification of the NameListService

--- AngularDialog/Scripts/NameListService.js
+++ AngularDialog/Scripts/NameListService.js
@@ -8,73 +8,35 @@
 
     app.service("nameListService", ["$resource", "$q", "$interpolate", "$window", function($resource, $q, $interpolate, $window) {
 
-        var url = $interpolate("http://{{hostname}}\\:{{port}}/Api/NameList/:id")({
+        var _url = $interpolate("http://{{hostname}}\\:{{port}}/Api/NameList/:id")({
             hostname: $window.location.hostname,
             port: $window.location.port
         });
 
-        var _resource = $resource(url, { id: "@Id" });
+        var _resource = $resource(_url, { id: "@Id" });
 
         this.query = function(successFn, errorFn) {
-
             successFn = successFn || angular.noop;
             errorFn = errorFn || angular.noop;
-
-            return _resource.query(
-                function(items) {
-                    successFn(items);
-                }, function(err) {
-                    errorFn(err);
-                });
+            return _resource.query(successFn, errorFn);
         };
 
         this.get = function(id, successFn, errorFn) {
-
             successFn = successFn || angular.noop;
             errorFn = errorFn || angular.noop;
-
-            return _resource.get(
-                {
-                    id: id
-                },
-                function(item) {
-                    successFn(item);
-                },
-                function(err) {
-                    errorFn(err);
-                });
+            return _resource.get({id: id}, successFn, errorFn);
         };
 
         this.save = function(item, successFn, errorFn) {
-
             successFn = successFn || angular.noop;
             errorFn = errorFn || angular.noop;
-
-            _resource.save(
-                item,
-                function(item2) {
-                    successFn(item2);
-                },
-                function(err) {
-                    errorFn(err);
-                });
+            _resource.save(item, successFn, errorFn);
         };
 
         this.remove = function(item, successFn, errorFn) {
-
             successFn = successFn || angular.noop;
             errorFn = errorFn || angular.noop;
-
-            _resource.remove(
-                {
-                    id: item.Id
-                },
-                function(x) {
-                    successFn(x);
-                },
-                function(err) {
-                    errorFn(err);
-                });
+            _resource.remove({ id: item.Id }, successFn, errorFn);
         };
     }]);
 } ());
